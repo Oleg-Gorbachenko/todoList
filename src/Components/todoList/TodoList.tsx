3ec523c7cec4e3599d0dfb35599897bdc9442c01
Oleg <viktorovich959595@mail.ru>
2022-02-17 11:13:34 +1000

    added the function of adding a task on the Enter button

--- src/Components/todoList/TodoList.tsx
+++ src/Components/todoList/TodoList.tsx
@@ -1,6 +1,5 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterValuesType} from "../../App";
-
 
 export type TaskType = {
     id: string
@@ -8,30 +7,36 @@ export type TaskType = {
     title: string
 }
 
-
 type PropsType = {
     title: string
     tasks: Array<TaskType>
     removeTask: (id: string) => void
     changeFilter: (value: FilterValuesType) => void
-    addTask: (title:string) => void
+    addTask: (title: string) => void
 }
 
 export const TodoList = (props: PropsType) => {
     let [title, setTitle] = useState('')
 
-    const onChangeInputHandler = (event:ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.currentTarget.value)
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+    const onKeyPressButtonHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === ('Enter')){
+            props.addTask(title)
+            setTitle('')
+        }
     }
 
     const onClickAddButton = () => {
         props.addTask(title)
+        setTitle('')
     }
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input value={title} onChange={onChangeInputHandler}/>
+                <input value={title} onChange={onChangeInputHandler} onKeyPress={onKeyPressButtonHandler}/>
                 <button onClick={onClickAddButton}>+</button>
             </div>
             <ul>
